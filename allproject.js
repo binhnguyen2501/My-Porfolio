fb60af707124d6912c3801b28c52c3d592bb5b32
Binh Nguyen <binhq6@gmail.com>
2020-12-22 17:29:16 +0700

    My Porfolio

--- allproject.js
+++ allproject.js
@@ -7,3 +7,13 @@ backtoWorkBtn.addEventListener('mouseleave', function(){
     cursor.classList.remove('link-grow_cursor')
     cursor2.classList.remove('link-grow_cursor2')
 })
+
+const nextProject = document.querySelector('.nextproject-caption h2');
+nextProject.addEventListener('mouseover', function(){
+    cursor.classList.add('link-grow_cursor')
+    cursor2.classList.add('link-grow_cursor2')
+})
+nextProject.addEventListener('mouseleave', function(){
+    cursor.classList.remove('link-grow_cursor')
+    cursor2.classList.remove('link-grow_cursor2')
+})
